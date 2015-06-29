ff5cca9cfdcfbb4da05cad48cc46d43624ebaf00
David Arcus <arcus.david@gmail.com>
2015-06-29 13:44:38 +1200

    add canvas and some basic commands from a modal

--- app/scripts/controllers/hair.js
+++ app/scripts/controllers/hair.js
@@ -8,5 +8,11 @@ angular.module('trumpApp').controller('HairController', function($scope, close) 
 //   console.log('dismissModal');
     close(); // close
  };
+  
+  $scope.addHairToCanvas = function(hairId) {
+    
+    console.log('hair id: ' + hairId);
+    
+  };
 
 });

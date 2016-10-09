abac875fb7453bb7459a751fcf112613daa10623
David Arcus <arcus.david@gmail.com>
2016-10-09 23:58:13 +1300

    remove erroneous reference to trump2 module

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'trump2',
+      module: 'trump',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));

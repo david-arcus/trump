d60bf6657eaaa6bd1612f6b9bf66b504dfe5cde7
David Arcus <david.arcus@colensobbdo.co.nz>
2017-07-06 18:12:33 +1200

    add assets

--- src/app/components/trumpCanvas/trumpCanvas.directive.js
+++ src/app/components/trumpCanvas/trumpCanvas.directive.js
@@ -31,7 +31,7 @@
       init();
 
       var vm = this;
-      
+
       // canvas elements
       var canvas, stageStatic, stageDrawing, flagBg;
       // asset loading elements
@@ -40,16 +40,16 @@
       var don, brush, hairCurlyBrunette, hairCurlyBlonde, hairCurlyGinger;
       // values sent from hair settings toolkit
       var hairLength, hairType, hairColour;
-      
+
       // vars to hold user selected elements
       var styles = [];
       var eyesFurtive, eyesSurprised;
       var hairGursling, hairHallary, hairLernal, hairOrlundo, hairPowell, hairFirrel;
       var mouthSad, mouthShouty;
-      
+
       // hold currently selected hairstyle, eyes and mouth
       var currentHair, currentEyes, currentMouth;
-      
+
       // broadcasted events
 
       $scope.$on('hairSettings', function(e, data) {
@@ -62,81 +62,81 @@
       });
 
       $scope.$on('clearHair', function(e, data) {
-        
+
         // remove drawn hair from canvas
         stageDrawing.clear();
         stageStatic.update();
-        
+
       });
 
-      
+
       $rootScope.$on('hairStyle', function(e, data) {
-        
+
         // add hair style to canvas
         stageStatic.removeChild(currentHair);
         stageStatic.addChild(styles[data]);
         currentHair = styles[data];
         stageStatic.update();
-        
+
       });
-      
+
       $rootScope.$on('eyeStyle', function(e, data) {
-        
+
         // add eye style to canvas
         stageStatic.removeChild(currentEyes);
         stageStatic.addChild(styles[data]);
         currentEyes = styles[data];
         stageStatic.update();
-        
+
       });
-      
+
       $rootScope.$on('mouthStyle', function(e, data) {
-        
+
         // add mouth style to canvas
         stageStatic.removeChild(currentMouth);
         stageStatic.addChild(styles[data]);
         currentMouth = styles[data];
         stageStatic.update();
-        
+
       });
-      
+
       $rootScope.$on('removeHairStyle', function(e, data) {
-        
+
         // make don bald
         stageStatic.removeChild(currentHair);
         stageStatic.update();
-        
+
       });
-      
+
       $rootScope.$on('removeEyeStyle', function(e, data) {
-        
+
         // default eyes
         stageStatic.removeChild(currentEyes);
         stageStatic.update();
-        
+
       });
-      
+
       $rootScope.$on('removeMouthStyle', function(e, data) {
-        
+
         // default mouth
         stageStatic.removeChild(currentMouth);
         stageStatic.update();
-        
+
       });
-      
+
       function getRandomInt(min, max) {
-        
+
         return Math.floor(Math.random() * (max - min + 1)) + min;
-        
+
       }
 
       function init() {
 
         stageStatic             = new createjs.Stage('the-don');
         stageDrawing            = new createjs.Stage('the-hair');
-        
+
         stageDrawing.autoClear  = false;
-        
+
         createjs.Touch.enable(stageDrawing, stageStatic);
 
 
@@ -145,28 +145,28 @@
         manifest = [
           {id: 'hairCurlyBrunette', src:'hair_curly_brunette.png'},
           {id: 'hairCurlyGinger', src:'hair_curly_ginger.png'},
-          
+
           {id: 'eyesFurtive', src:'tools/eyes/furtive.png'},
           {id: 'eyesSurprised', src:'tools/eyes/surprised.png'},
-          
+
           {id: 'hairGursling', src:'tools/hairstyles/gursling.png'},
           {id: 'hairHallary', src:'tools/hairstyles/hallary.png'},
           {id: 'hairLernal', src:'tools/hairstyles/lernal.png'},
           {id: 'hairOrlundo', src:'tools/hairstyles/orlundo.png'},
           {id: 'hairPowell', src:'tools/hairstyles/powell.png'},
           {id: 'hairFirrel', src:'tools/hairstyles/wall_firrel.png'},
-          
+
           {id: 'mouthSad', src:'tools/mouth/sad.png'},
           {id: 'mouthShouty', src:'tools/mouth/shouty.png'},
           {id: 'flagBg', src:'flag-bg.jpg'},
-          
-          {id: 'don', src:'don_bald.png'}
+
+          {id: 'don', src:'turk.jpg'}
         ];
 
         loader = new createjs.LoadQueue(false);
         loader.addEventListener('complete', handleComplete);
         loader.loadManifest(manifest, true, 'assets/images/');
-        
+
       }
 
       function handleComplete() {
@@ -175,7 +175,7 @@
         flagBg              = new createjs.Bitmap(loader.getResult('flagBg'));
         hairCurlyBrunette   = new createjs.Bitmap(loader.getResult('hairCurlyBrunette'));
         hairCurlyGinger     = new createjs.Bitmap(loader.getResult('hairCurlyGinger'));
-        
+
         eyesFurtive         = new createjs.Bitmap(loader.getResult('eyesFurtive'));
         eyesSurprised       = new createjs.Bitmap(loader.getResult('eyesSurprised'));
         hairGursling        = new createjs.Bitmap(loader.getResult('hairGursling'));
@@ -186,7 +186,7 @@
         hairFirrel          = new createjs.Bitmap(loader.getResult('hairFirrel'));
         mouthSad            = new createjs.Bitmap(loader.getResult('mouthSad'));
         mouthShouty         = new createjs.Bitmap(loader.getResult('mouthShouty'));
-        
+
         styles = {
           'eyesFurtive': eyesFurtive,
           'eyesSurprised': eyesSurprised,
@@ -199,14 +199,14 @@
           'mouthSad': mouthSad,
           'mouthShouty': mouthShouty,
         }
-        
+
         stageStatic.addChild(flagBg);
         stageStatic.addChild(don);
         stageStatic.update();
 
         stageDrawing.addEventListener('stagemousedown', handleMouseDown);
         stageDrawing.addEventListener('stagemouseup', handleMouseUp);
-        
+
       }
 
       function handleMouseDown(event) {
--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -1,4 +1,3 @@
-/* global */
 (function() {
   'use strict';
 

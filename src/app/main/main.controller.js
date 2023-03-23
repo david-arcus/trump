332d9cb7fba72ff163b64dea3ce40b70d5fb7f01
David Arcus <arcus.david@gmail.com>
2023-03-23 18:09:03 +1100

    fix share with modal step

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,10 +6,39 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController($sce) {
 
-    // entry point
     var vm = this;
+
+    function prepareImage() {
+
+      // TODO: make into directive
+      var canvas = document.getElementById('the-don');
+      var hair = document.getElementById('the-hair');
+      var output = document.createElement('canvas');
+
+      output.width = canvas.width;
+      output.height = canvas.height;
+
+      var ctx = output.getContext("2d");
+
+      // add hair canvas to the don canvas
+      ctx.drawImage(canvas, 0, 0)
+      ctx.drawImage(hair, 0, 0)
+
+      return $sce.trustAsResourceUrl(output.toDataURL('image/jpeg', 0.8));
+
+    }
+    
+    vm.share = function() {
+      
+      vm.sharing = true;
+      
+      var image = prepareImage();
+        
+      vm.download = image;
+            
+    }
 
   }
   

931471ba6db408797d5eee2cb0cf7f5b2cdf6fb8
David Arcus <arcus.david@gmail.com>
2023-03-23 16:26:22 +1100

    remove existing share button from main controller

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,52 +6,10 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($log, ModalService, Facebook, Api) {
+  function MainController() {
 
+    // entry point
     var vm = this;
-    var shareImage;
-    
-    vm.uploaded = false;
-    vm.share = function() {
-      
-      vm.loading = true;
-      
-      var imageName;
-      var canvas = document.getElementById('the-don');
-      var hair = document.getElementById('the-hair');
-      var ctx = canvas.getContext("2d");
-      
-      // add hair canvas to the don canvas
-      ctx.drawImage(hair,0,0)
-      
-      var image = canvas.toDataURL('image/jpeg');
-
-      Api.uploadImage(image).then(function(result) {
-
-        vm.loading = false;
-        vm.uploaded = true;
-        
-        $log.debug(result.data);
-
-        shareImage = result.data.image_path;
-        
-        vm.sharePath = result.data.image_path;
-        
-        
-
-      });
-    
-    }
-    
-    vm.shareFacebook = function() {
-      FB.ui({
-        method: 'feed',
-        link: 'http://www.trumpsformation.com',
-        caption: 'Trumpsformation',
-        description: 'Give Don the hair he needs.',
-        picture: vm.sharePath
-      }, function(response){});
-    }
 
   }
   

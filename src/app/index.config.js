30d5fc2e94c2ac70bb70bd33253a05a603f679e6
David Arcus <arcus.david@gmail.com>
2023-03-23 16:54:20 +1100

    allow base64 images as href attributes

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,12 +6,16 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, FacebookProvider) {
+  function config($logProvider, FacebookProvider, $compileProvider) {
     // Enable log
     $logProvider.debugEnabled(true);
 
     // init facebook app
     FacebookProvider.init('302835310090558');
+
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|data):/);
+
+
   }
 
 })();
